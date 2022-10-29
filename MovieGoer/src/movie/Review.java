4feb7eae6dee17a3b76cbe84edd0d1192e8cd844
Wei Chuan <43843639+weix1233@users.noreply.github.com>
2022-10-29 15:31:11 +0800

    Create Review.java

---
+++ MovieGoer/src/movie/Review.java
@@ -0,0 +1,25 @@
+
+public class Review {
+	private String review;
+	private int rating;
+	private String reviewerName;
+	
+	public String getReview() {
+		return this.review;
+	}
+	public int getRating() {
+		return this.rating;
+	}
+	public String getReviewer() {
+		return this.reviewerName;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public void setReviewer(String reviewer) {
+		this.reviewerName = reviewer;
+	}
+}
