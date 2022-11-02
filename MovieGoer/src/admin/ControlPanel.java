bd2a0e48629f9c167ab24dfefe8e1404e2f8e61e
Tony <33109090+ttan-999@users.noreply.github.com>
2022-11-02 18:12:51 +0800

    Able to read Movie CSV file

--- MovieGoer/src/admin/ControlPanel.java
+++ MovieGoer/src/admin/ControlPanel.java
@@ -1,10 +1,15 @@
 package admin;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import movie.Movie;
 
 public class ControlPanel {
-	// private MovieListing[] listing;
 
 	private static void WelcomeBanner() {
 		System.out.println("Welcome to the movie admin console. You can edit movie information here");
@@ -15,10 +20,36 @@ public class ControlPanel {
 		WelcomeBanner();
 		StaffLogin sl = new StaffLogin();
 		int userType = 0;
-		// Guest = 1, Members = 2, Admin = 3, Login Errors = -1
+		// Guest = 1, Members = 2, Admin = 3, Login Error = -1 user is forced to login
+		// again
 		do {
-			userType = sl.validate();
+			// userType = sl.validate();
+			userType = 3;
 		} while (userType == -1);
+
+		switch (userType) {
+		case 1:
+			// guest
+			break;
+		case 2:
+			// member
+			break;
+		case 3:
+			// admin
+			System.out.println("Find a movie!");
+			String movieFilePath = "C:\\Users\\tanju\\Desktop\\movie\\movie.txt";
+			List<Movie> beans = new CsvToBeanBuilder(new FileReader(movieFilePath)).withType(Movie.class).build()
+					.parse();
+			String watch = "Life Of Pi";
+			Movie ref = null;
+			for (int i = 0; i < beans.size(); i++) {
+				if (watch.equals(beans.get(i).getMovieTitle())) {
+					ref = beans.get(i);
+				}
+			}
+			System.out.println(ref.getCast());
+			break;
+		}
 	}
 
 }
