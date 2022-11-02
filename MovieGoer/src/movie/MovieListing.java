ed97e255beb08e341c5b2cfcbd6e82f89d2cb782
Tony <33109090+ttan-999@users.noreply.github.com>
2022-11-02 15:14:16 +0800

    Add OpenCSV library and dependencies

--- MovieGoer/src/movie/MovieListing.java
+++ MovieGoer/src/movie/MovieListing.java
@@ -1,7 +1,7 @@
 package movie;
 
 public class MovieListing {
-	public String testGIthubChange;
+
 	private enum screenType {
 		TWO_D, THREE_D
 	};
@@ -59,23 +59,25 @@ public class MovieListing {
 	public void setAgeRate(ageRating ageRate) {
 		this.ageRate = ageRate;
 	}
-	
+
 	public double getOverallRating() {
 		double total = 0;
-		for(int i=0; i<reviews.length; i++) {
-			total = total+reviews[i].getRating();
+		for (int i = 0; i < reviews.length; i++) {
+			total = total + reviews[i].getRating();
 		}
-		return Math.round(total/reviews.length*10)/10;
+		return Math.round(total / reviews.length * 10) / 10;
 	}
+
 	public void displayReviews() {
-		for(int i=0; i<reviews.length; i++) {
+		for (int i = 0; i < reviews.length; i++) {
 			System.out.println(reviews[i].getReview());
 		}
 	}
+
 	public void addReview(Review review) {
-		Review[] temp = new Review[reviews.length+1];
+		Review[] temp = new Review[reviews.length + 1];
 		System.arraycopy(reviews, 0, temp, 0, reviews.length);
-		temp[temp.length-1] = review;
+		temp[temp.length - 1] = review;
 		reviews = temp;
 	}
 
