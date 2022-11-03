bcd3ecf67dc063dff6bd42c6aeabd96c9708780f
weix1 <weix1@LAPTOP-7MOHFDB2>
2022-11-03 10:00:06 +0800

    Updated reviews to List type

--- MovieGoer/src/movie/Movie.java
+++ MovieGoer/src/movie/Movie.java
@@ -18,7 +18,7 @@ public class Movie {
 	// @CsvBindAndSplitByName(elementType = String.class, splitOn = ",")
 	// private String[] cast;
 	// private int castPointer;
-	private Review[] reviews;
+	private List<Review> reviews = new ArrayList<Review>();
 
 	public Movie() {
 	};
@@ -28,7 +28,6 @@ public class Movie {
 		this.synopis = synopis;
 		this.director = director;
 		this.cast = cast;
-		Review[] reviews = new Review[1];
 	}
 
 	public String getMovieTitle() {
@@ -58,33 +57,30 @@ public class Movie {
 	public List<String> getCast() {
 		return cast;
 	}
-	public Review[] getReviews() {
+	public List<Review> getReviews() {
 		return reviews;
 	}
 
-	public void setReviews(Review[] reviews) {
+	public void setReviews(List<Review> reviews) {
 		this.reviews = reviews;
 	}
 
 	public double getOverallRating() {
 		double total = 0;
-		for (int i = 0; i < reviews.length; i++) {
-			total = total + reviews[i].getRating();
+		for (int i = 0; i < reviews.size(); i++) {
+			total = total + reviews.get(i).getRating();
 		}
-		return Math.round(total / reviews.length * 10) / 10;
+		return Math.round(total / reviews.size() * 10) / 10;
 	}
 
 	public void displayReviews() {
-		for (int i = 0; i < reviews.length; i++) {
-			System.out.println(reviews[i].getReview());
+		for (int i = 0; i < reviews.size(); i++) {
+			System.out.println(reviews.get(i).getReview());
 		}
 	}
 
 	public void addReview(Review review) {
-		Review[] temp = new Review[reviews.length + 1];
-		System.arraycopy(reviews, 0, temp, 0, reviews.length);
-		temp[temp.length - 1] = review;
-		reviews = temp;
+		reviews.add(review);
 	}
 	/*
 	 * public String[] getCast() { return cast; }
