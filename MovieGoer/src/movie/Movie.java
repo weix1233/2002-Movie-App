0923b28d6f77e07642f5bdb693212355e0b4cf16
weix1 <weix1@LAPTOP-7MOHFDB2>
2022-11-03 09:38:14 +0800

    Moved reviews from MovieListing to Movie

--- MovieGoer/src/movie/Movie.java
+++ MovieGoer/src/movie/Movie.java
@@ -18,6 +18,7 @@ public class Movie {
 	// @CsvBindAndSplitByName(elementType = String.class, splitOn = ",")
 	// private String[] cast;
 	// private int castPointer;
+	private Review[] reviews;
 
 	public Movie() {
 	};
@@ -27,6 +28,7 @@ public class Movie {
 		this.synopis = synopis;
 		this.director = director;
 		this.cast = cast;
+		Review[] reviews = new Review[1];
 	}
 
 	public String getMovieTitle() {
@@ -56,7 +58,34 @@ public class Movie {
 	public List<String> getCast() {
 		return cast;
 	}
+	public Review[] getReviews() {
+		return reviews;
+	}
 
+	public void setReviews(Review[] reviews) {
+		this.reviews = reviews;
+	}
+
+	public double getOverallRating() {
+		double total = 0;
+		for (int i = 0; i < reviews.length; i++) {
+			total = total + reviews[i].getRating();
+		}
+		return Math.round(total / reviews.length * 10) / 10;
+	}
+
+	public void displayReviews() {
+		for (int i = 0; i < reviews.length; i++) {
+			System.out.println(reviews[i].getReview());
+		}
+	}
+
+	public void addReview(Review review) {
+		Review[] temp = new Review[reviews.length + 1];
+		System.arraycopy(reviews, 0, temp, 0, reviews.length);
+		temp[temp.length - 1] = review;
+		reviews = temp;
+	}
 	/*
 	 * public String[] getCast() { return cast; }
 	 * 
--- MovieGoer/src/movie/MovieListing.java
+++ MovieGoer/src/movie/MovieListing.java
@@ -29,7 +29,6 @@ public class MovieListing {
 	private dayOfWeek day;
 	private int showtime;
 	private int cinemaHall;
-	private Review[] reviews;
 	private Seat[][] seat = new Seat[9][13];
 
 	public MovieListing(int movieListingID, Movie movie, screenType type, showingStatus status, ageRating ageRate,
@@ -42,7 +41,6 @@ public class MovieListing {
 		this.day = day;
 		this.showtime = showtime;
 		this.cinemaHall = cinemaHall;
-		Review[] reviews = new Review[1];
 	}
 
 	public int getMovieListingID() {
@@ -109,35 +107,6 @@ public class MovieListing {
 
 	public void setCinemaHall(int cinemaHall) {
 		this.cinemaHall = cinemaHall;
-	}
-
-	public Review[] getReviews() {
-		return reviews;
-	}
-
-	public void setReviews(Review[] reviews) {
-		this.reviews = reviews;
-	}
-
-	public double getOverallRating() {
-		double total = 0;
-		for (int i = 0; i < reviews.length; i++) {
-			total = total + reviews[i].getRating();
-		}
-		return Math.round(total / reviews.length * 10) / 10;
-	}
-
-	public void displayReviews() {
-		for (int i = 0; i < reviews.length; i++) {
-			System.out.println(reviews[i].getReview());
-		}
-	}
-
-	public void addReview(Review review) {
-		Review[] temp = new Review[reviews.length + 1];
-		System.arraycopy(reviews, 0, temp, 0, reviews.length);
-		temp[temp.length - 1] = review;
-		reviews = temp;
 	}
 	
 	public void showSeats(){
