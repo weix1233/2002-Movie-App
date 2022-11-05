c3741dfd365e685f3a0bd04c63edb8a856ec817f
whysilon <valencino1306@gmail.com>
2022-11-05 20:18:18 +0800

    Commiting

--- MovieGoer/src/admin/CustomerApp.java
+++ MovieGoer/src/admin/CustomerApp.java
@@ -1,5 +1,5 @@
 package admin;
-
+ 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
@@ -11,22 +11,36 @@ import movie.*;
 
 public class CustomerApp {
 	private Cinema currentCinema;
-	public void chooseCinema() throws IllegalStateException, FileNotFoundException{
+	public CustomerApp() throws IllegalStateException, FileNotFoundException {
+		this.currentCinema = chooseCinema();
+	}
+	public Cinema chooseCinema() throws IllegalStateException, FileNotFoundException{
 		Scanner sc = new Scanner(System.in);
 		String cinemaFileName = "C:\\Users\\Valen\\git\\2002-Movie-App-branch\\MovieGoer\\database\\cinema\\cinema.csv";
 		List<Cinema> cinemaBeans = new CsvToBeanBuilder(new FileReader(cinemaFileName)).withType(Cinema.class).build()
 				.parse();
 		System.out.print("Select location\n(1) jurong (2) orchard (3) yishun: ");
 		int locationID = sc.nextInt();
-		currentCinema = cinemaBeans.get(locationID);
+		return cinemaBeans.get(locationID-1);
 	}
 	public void BookMovie(MovieListing ml) throws IllegalStateException, FileNotFoundException {
-		showMovies(currentCinema.getML());
-		Booking b = new Booking(ml,currentCinema.getCinemaID());
-		b = b.displayBooking();
+		//showMovies(currentCinema.getML());
+		Booking b = new Booking(ml,currentCinema);
+		//b = b.displayBooking();
+		
+	}
+	public void displayMenu() {
+		String buf = "";
+		int count = (50 - this.currentCinema.getName().length() - 9)/2;
+		for(int i = 0;i < count;i++) buf = buf + "=";
+		System.out.println("==================================================");
+		//System.out.printf("%d",count);
+		if(currentCinema.getName().length() % 2 == 0) System.out.print("=");
+		System.out.printf("%s Cathay %s %s\n",buf,currentCinema.getName(),buf);
+		System.out.println("==================================================");
 		
 	}
 	public static void showMovies(List<MovieListing> mListings) {
-		displayUniqueMovie();
+		//displayUniqueMovie();
 	}
 }
