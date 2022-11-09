10f4fee1b90c62149793ecd9efc2cf23482a8bc9
Tony <33109090+ttan-999@users.noreply.github.com>
2022-11-09 16:05:19 +0800

    final ver as of 9/11

--- MovieGoer/src/entity/Review.java
+++ MovieGoer/src/entity/Review.java
@@ -4,22 +4,33 @@ public class Review {
 	private String review;
 	private int rating;
 	private String reviewerName;
-	
+
+	public Review(String review, int rating, String reviewerName) {
+		this.review = review;
+		this.rating = rating;
+		this.reviewerName = reviewerName;
+	}
+
 	public String getReview() {
 		return this.review;
 	}
+
 	public int getRating() {
 		return this.rating;
 	}
+
 	public String getReviewer() {
 		return this.reviewerName;
 	}
+
 	public void setReview(String review) {
 		this.review = review;
 	}
+
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
+
 	public void setReviewer(String reviewer) {
 		this.reviewerName = reviewer;
 	}
